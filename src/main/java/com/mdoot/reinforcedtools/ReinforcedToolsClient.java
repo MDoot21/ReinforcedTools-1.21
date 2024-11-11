56328635b45ae73cc607a384e17d734bb8ea5c26
MDoot21 <azar.adiga1@hotmail.com>
2024-11-11 22:51:25 +1100

    Initial Commit2

---
+++ src/main/java/com/mdoot/reinforcedtools/ReinforcedToolsClient.java
@@ -0,0 +1,10 @@
+package com.mdoot.reinforcedtools;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ReinforcedToolsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
